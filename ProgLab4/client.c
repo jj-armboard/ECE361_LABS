9c99a499fd344b664941cd14aee8fa86c2e9d950
mckelvi7 <callum.mckelvie@mail.utoronto.ca>
2022-03-20 23:40:29 -0400

    Implemented simple TCP connectivity

--- ProgLab4/client.c
+++ ProgLab4/client.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <math.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
-   printf("Hello, World!\n");
+   int sockfd = 0;
+   int yes = 1;
+
+   struct addrinfo serverInfo, *serverInfoPtr;
+   struct sockaddr_storage senderInfo;
+
+   socklen_t addressLength = sizeof(senderInfo);
+
+   // Initialize serverInfo
+   serverInfo.ai_flags = AI_PASSIVE;
+   serverInfo.ai_family = AF_INET;
+   serverInfo.ai_socktype = SOCK_STREAM;
+
+   serverInfo.ai_protocol = 0;
+   serverInfo.ai_addrlen = 0;
+   serverInfo.ai_addr = NULL;
+   serverInfo.ai_canonname = NULL;
+   serverInfo.ai_next = NULL;
+
+   // Resolves a hostname into an address
+   getaddrinfo(argv[1], argv[2], &serverInfo, &serverInfoPtr);
+
+   // Create a socket
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+   connect(sockfd, serverInfoPtr->ai_addr,  sizeof(*(serverInfoPtr->ai_addr)));
+
+   char buff[100] = "This is a test";
+
+   write(sockfd, buff, sizeof(buff));
+
    return 0;
 }
--- ProgLab4/server.c
+++ ProgLab4/server.c
@@ -1,8 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <math.h>
 
-int main() {
+#define BACKLOG 10 // how many pending connections queue will hold
 
-   printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+
+   int sockfd = 0;
+   int newSockfd = 0;
+   int yes = 1;
+
+   struct addrinfo serverInfo, *serverInfoPtr;
+   struct sockaddr_storage senderInfo;
+
+   socklen_t addressLength = sizeof(senderInfo);
+
+   // Initialize serverInfo
+   serverInfo.ai_flags = AI_PASSIVE;
+   serverInfo.ai_family = AF_INET;
+   serverInfo.ai_socktype = SOCK_STREAM;
+
+   serverInfo.ai_protocol = 0;
+   serverInfo.ai_addrlen = 0;
+   serverInfo.ai_addr = NULL;
+   serverInfo.ai_canonname = NULL;
+   serverInfo.ai_next = NULL;
+
+   // Resolves a hostname into an address
+   getaddrinfo(NULL, argv[1], &serverInfo, &serverInfoPtr);
+
+   // Create a socket
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+   //setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+   // Bind it to the port we passed in to getaddrinfo()
+   bind(sockfd, serverInfoPtr->ai_addr, sizeof(*(serverInfoPtr->ai_addr)));
+
+   printf("Server Listening On Port: %s\n", argv[1]);
+
+   listen(sockfd, BACKLOG);
+
+   addressLength = sizeof(senderInfo);
+
+   newSockfd = accept(sockfd, (struct sockaddr*)&senderInfo, &addressLength);
+
+   //----- Testing -----//
+
+   char buff[100];
+
+   read(newSockfd, buff, sizeof(buff));
+
+   printf("Server Received: \"%s\"\n", buff);
+
    return 0;
 }
+
